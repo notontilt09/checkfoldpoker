0c452288007e2bc117a03efe760915d3539aaa0c
Daniel Weinman <namniewd@gmail.com>
2019-09-21 10:52:00 -0400

    added images for cards, begun work on displaying them

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+import images from './assets/images.js';
+import club from './assets/PNG-cards-1.3/2_of_clubs.png'
+
+const App = () => {
+  const [p1bottom, setp1bottom] = useState([]);
+  const [p1mid, setp1mid] = useState([]);
+  const [p1top, setp1top] = useState([]);
+  const [p1discards, setp1discards] = useState([]);
+  const [p2bottom, setp2bottom] = useState([]);
+  const [p2mid, setp2mid] = useState([]);
+  const [p2top, setp2top] = useState([]);
+  const [p2discards, setp2discards] = useState([]);
+
+
+  // get deck from backend on mount
+  useEffect(() => {
+    axios.get('http://localhost:5000/shuffle')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
