c8277b1f89467aae50f943a5f7e8c072448803a2
Daniel Weinman <namniewd@gmail.com>
2019-10-31 13:56:22 -0400

    some comments on the register router

--- backend/auth/authRouter.js
+++ backend/auth/authRouter.js
@@ -10,22 +10,25 @@ router.post('/register', async (req, res) => {
   } else {
     // generate hash from users's pw
     const hash = bcrypt.hashSync(user.password, 8);
+    // set the user's pw to the hash
     user.password = hash
     
     // try adding a user to the database
     try {
+      // grab the mongoDB and the users table
       const db = mongo.getDB();
       const users = db.collection('users');
       // check to make sure user isn't already in the db, before adding
       const existingUser = await users.findOne({ username: user.username });
-
+      // if username exists, return 400 and corresponding message
       if (existingUser) {
         res.status(400).json({ error: `user ${user.username} already exists.` })
+        // otherwise add the user to the db
       } else {
         await users.insertOne(user)
         res.status(201).json({ message: `${user.username} registered.`})
       }
-      
+
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: 'Error registering user.'})
