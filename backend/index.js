efa5de0f38a0ed559c62e1d480c54854e4f877fc
Matt Horstman <matt.horstman@gmail.com>
2019-11-04 17:39:05 -0500

    Adds babel to toolchain

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const socketIO = require('socket.io');
-const http = require('http');
+// const socketIO = require('socket.io');
+import socketIO from 'socket.io';
+import http from 'http';
 
 const app = require('./api/server');
 const server = http.createServer(app);
 const io = socketIO(server);
 const connection = require('./api/WebSocket/socket-io.js')(io);
-
 
 const port = process.env.PORT || 5000;
 
