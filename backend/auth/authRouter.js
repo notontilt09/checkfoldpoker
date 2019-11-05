9d622ffe4a168294c64878534bb006c8aa321acf
Daniel Weinman <namniewd@gmail.com>
2019-11-05 13:23:09 -0500

    removed ws and babel, register route now sending token on success

--- backend/auth/authRouter.js
+++ backend/auth/authRouter.js
@@ -5,20 +5,20 @@ const bcrypt = require('bcryptjs');
 const mongo = require('../db/db.js');
 
 router.post('/register', async (req, res) => {
-  console.log(req.body);
-  const user = req.body;
+  // console.log(req.body);
+  let {username, password} = req.body;
   // validate input to contain username > 3 chars and password > 8 chars.  Front-end validation as well to make sure passwords match and are strong enough.
-  if (!user.userName || !user.password) {
+  if (!username || !password) {
     res.status(400).json({message: 'Username and password required.'});
-  } else if (user.userName.length < 3) {
+  } else if (username.length < 3) {
     res.status(400).json({message: 'Username must be at least 3 characters.'});
-  } else if (user.password.length < 8) {
+  } else if (password.length < 8) {
     res.status(400).json({message: 'Password must be at least 8 characters.'});
   } else {
     // generate hash from users's pw
-    const hash = bcrypt.hashSync(user.password, 8);
+    const hash = bcrypt.hashSync(password, 8);
     // set the user's pw to the hash
-    user.password = hash;
+    password = hash;
 
     // try adding a user to the database
     try {
@@ -26,14 +26,19 @@ router.post('/register', async (req, res) => {
       const db = mongo.getDB();
       const users = db.collection('users');
       // check to make sure user isn't already in the db, before adding
-      const existingUser = await users.findOne({username: user.userName});
+      const existingUser = await users.findOne({username: username});
       // if username exists, return 400 and corresponding message
       if (existingUser) {
-        res.status(400).json({error: `user ${user.userName} already exists.`});
+        res.status(400).json({error: `user ${username} already exists.`});
         // otherwise add the user to the db
       } else {
-        await users.insertOne(user);
-        res.status(201).json(user);
+        await users.insertOne({username, password});
+        const user = await users.findOne({username: username})
+        const token = tokenService.generateToken(user);
+        res.status(201).json({
+          message: `Successfully registered user ${username}`,
+          token
+        });
       }
     } catch (error) {
       console.log(error);
@@ -43,24 +48,24 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  const {userName, password} = req.body;
+  const {username, password} = req.body;
 
   try {
     const db = mongo.getDB();
     const users = db.collection('users');
-    const user = await users.findOne({userName: userName});
+    const user = await users.findOne({username: username});
 
     if (user && bcrypt.compareSync(password, user.password)) {
       const token = tokenService.generateToken(user);
       res.status(200).json({
-        message: `Welcome ${user.userName}!`,
+        message: `Welcome ${user.username}!`,
         token,
       });
     } else {
       res.status(401).json({message: 'Invalid credentials!'});
     }
   } catch (error) {
-    res.status(500).json({message: 'Erro retreiving user info.'});
+    res.status(500).json({message: 'Error retreiving user info.'});
   }
 });
 
